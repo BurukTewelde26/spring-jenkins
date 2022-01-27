dcef92edeed03efb825847faf12d3548c46ff062
Bruk <buruktewelde@gmail.com>
2022-01-27 15:39:36 -0600

    first commit

---
+++ spring-jenkins/spring-jenkins/src/test/java/com/javatechie/jenkin/api/SpringJenkinsApplicationTests.java
@@ -0,0 +1,36 @@
+package com.javatechie.jenkin.api;
+
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.Assert;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class SpringJenkinsApplicationTests {
+	
+	static Logger logger  = LoggerFactory.getLogger( SpringJenkinsApplicationTests.class);
+
+	@Test
+	void contextLoads() {
+	// String expected = "true";
+	 //String actual = "true";
+		logger.info("Test Case executing");
+	 assertEquals(true,true);
+	}
+
+	private boolean assertEquals(boolean b, boolean c) {
+		// TODO Auto-generated method stub
+		if(b == c){
+			return true;
+		}else{
+			return false;
+		}
+	}
+
+
+}
